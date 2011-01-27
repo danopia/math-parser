1497bcc028a357ab38149fef37f1495e9588987e
Daniel Lamando <me@danopia.net>
2011-01-26 22:38:30 -0500

    Might want the variable class

---
+++ ast/variable.rb
@@ -0,0 +1,32 @@
+require 'ast/node'
+
+module AST
+  class Variable < Node
+    attr_accessor :letter
+    
+    def initialize letter
+      @letter = letter
+    end
+    
+    def * other
+      if other.is_a?(Variable) && @letter == other.letter
+        AST::Operation.new(self, :^, '2')
+      else
+        super
+      end
+    end
+    
+    def hash; @letter.hash; end
+    
+    def monomial?; true; end
+    def constant?; false; end
+    
+    def to_s; @letter.to_s; end
+    #def to_i; @letter.to_i; end
+    
+    def inspect
+      "#<AST::Variable #{@letter}>"
+    end
+  end
+end
+
