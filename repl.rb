ef2cdc8c71f1a23656261e776da0b343149ee297
Daniel Lamando <me@danopia.net>
2011-01-26 23:35:34 -0500

    Some REPL work

--- repl.rb
+++ repl.rb
@@ -2,20 +2,30 @@ require './parser'
 
 parser = MathParser.new
 
+trap('INT') do
+  puts if STDOUT.tty?
+  exit
+end
+
 while true
+  if STDOUT.tty?
     print '>> '
     STDOUT.flush
-    
-    begin
-      res = parser.parse(gets.chomp)
-    rescue ParseError => e
-      puts e.message
-    end
-    
-    begin
-      puts "=> #{res.to_i || res}"
-    rescue => e
-      puts e, e.backtrace
-    end
+  end
+  
+  line = gets
+  exit unless line
+  next if line.empty?
+  begin
+    res = parser.parse(line.chomp)
+  rescue ParseError => e
+    puts e.message
+  end
+  
+  begin
+    puts "=> #{res.to_i || res}"
+  rescue => e
+    puts e, e.backtrace
+  end
 end
 
